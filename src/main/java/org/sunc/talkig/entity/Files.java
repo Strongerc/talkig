d72a467ed7d56d2cc317f928d4db3bfcdcd16749
my <206214729@qq.com>
2023-07-09 14:46:52 +0800

    修改上传图片

--- src/main/java/org/sunc/talkig/entity/Files.java
+++ src/main/java/org/sunc/talkig/entity/Files.java
@@ -1,7 +1,5 @@
 package org.sunc.talkig.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -15,7 +13,6 @@ import lombok.Data;
 @TableName("sys_file")
 public class Files {
 
-    @TableId(type = IdType.AUTO)
     private Integer id;
     private String name;
     private String type;
